02730c1e4f6a4549821e3efc8936712aa76752b6
David Dev <davidanderson04@live.com>
2022-01-06 22:15:59 -0300

    refatorando assets

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+function mostrar() {
+    document.getElementById("aparecer0").style.display = "block";
+    document.getElementById("aparecer0").style.visibility = "visible";
+    document.getElementById("aparecer1").style.display = "block";
+    document.getElementById("aparecer1").style.visibility = "visible";
+    document.getElementById("aparecer2").style.display = "block";
+    document.getElementById("aparecer2").style.visibility = "visible";
+    document.getElementById("aparecer3").style.display = "block";
+    document.getElementById("aparecer3").style.visibility = "visible";
+    document.getElementById("aparecer4").style.display = "block";
+    document.getElementById("aparecer4").style.visibility = "visible";
+    document.getElementById("aparecer5").style.display = "block";
+    document.getElementById("aparecer5").style.visibility = "visible";
+    document.getElementById("aparecer6").style.display = "block";
+    document.getElementById("aparecer6").style.visibility = "visible";
+    document.getElementById("aparecer7").style.display = "block";
+    document.getElementById("aparecer7").style.visibility = "visible";
+  }
+  var tempo = 420 //tempo em segundos 
+  var res = tempo * 1000
+
+  setTimeout("mostrar()", res);
